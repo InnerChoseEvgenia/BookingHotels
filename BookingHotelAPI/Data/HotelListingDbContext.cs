40ff5913b45280ced1968219ff248c562cfea78e
InnerChoseEvgenia <evgenialukashen@gmail.com>
2025-12-27 16:52:09 +0300

    Добавьте файлы проекта.

---
+++ BookingHotelAPI/Data/HotelListingDbContext.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace BookingHotelAPI.Data;
+
+public class HotelBookingDbContext(DbContextOptions<HotelBookingDbContext> options) 
+    : DbContext(options)
+{
+    public DbSet<Country> Countries { get; set; }
+    public DbSet<Hotel> Hotels { get; set; }
+    
+
+    //protected override void OnModelCreating(ModelBuilder builder)
+    //{
+    //    base.OnModelCreating(builder);
+    //    builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+    //    builder.Entity<Country>()
+    //        .HasIndex(c => c.Name)
+    //        .HasDatabaseName("IX_Countries_Name");
+
+    //    builder.Entity<Country>()
+    //        .HasIndex(c => c.ShortName)
+    //        .HasDatabaseName("IX_Countries_ShortName");
+
+    //    builder.Entity<Hotel>()
+    //       .HasIndex(h => h.Name)
+    //       .HasDatabaseName("IX_Hotels_Name");
+
+    //    builder.Entity<Hotel>()
+    //        .HasIndex(h => h.CountryId)
+    //        .HasDatabaseName("IX_Hotels_CountryId");
+
+    //    builder.Entity<Hotel>()
+    //        .HasIndex(h => new { h.CountryId, h.Rating })
+    //        .HasDatabaseName("IX_Hotels_CountryId_Rating");
+    //}
+}
