bbd2253b561752201262e3660e49a745de48d701
InnerChoseEvgenia <evgenialukashen@gmail.com>
2026-01-04 20:32:51 +0300

    Added InMemoryCashe to the CountryController

---
+++ src/BookingHotelAPI.Common/Constants/CacheConstants.cs
@@ -0,0 +1,17 @@
+﻿namespace BookingHotelAPI.Common.Constants;
+
+public static class CacheConstants
+{
+    // Output Cache Policy Names
+    public const string AuthenticatedUserCachingPolicy = "AuthenticatedUserCachingPolicy";
+
+    public const string AuthenticatedUserCachingPolicyTag = "authpolicy-";
+
+    public const string CountrySingleCacheName = "country_";
+    public const string CountryListCacheName = "countries_list_";
+
+    // Cache Durations (in seconds)
+    public const int ShortDuration = 60; // 1 minute
+    public const int MediumDuration = 300; // 5 minutes
+    public const int LongDuration = 900; // 15 minutes
+}
